beac62951c1423dab98af70a3b429cc2f2c621ce
Jedd Saliba <jeddsaliba@gmail.com>
2022-05-23 21:18:11 +0800

    Update User.php

--- src/Models/User.php
+++ src/Models/User.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
